f8841db87741e6e16a302cd3f1a5aaf2f3be968b
dii <dstephenhono@gmail.com>
2020-06-09 11:07:25 -0400

    n/a.

--- src/dll.c
+++ src/dll.c
@@ -12,9 +12,10 @@ node_t* new_node(node_t * p, node_t * n, void * d) {
   return newest;
 }
 
+//Note: This dll uses head and tail sentinel nodes. 
 void init_dll(dll_t *list) {
   list->size = 0;
   list->header = new_node(NULL, NULL, NULL); // head sentinel
   list->trailer = new_node(list->header, NULL, NULL); // tail sentinel
-  list->header->next = list->trailer;
+  list->header->next = list->trailer; // header -> trailer
 }

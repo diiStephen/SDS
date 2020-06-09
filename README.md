99e135f5cf7237d490c24aef53954ff210cfc57a
dii <dstephenhono@gmail.com>
2020-06-09 10:24:56 -0400

    building prototypes for dll.

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Overview
 
-A collection of standard data structures implemented in C.
+A collection of standard data structures implemented in C. 
 
 # Supported Data Structures
 
 Data Structure | Source | Comments
 ---------------|--------|---------
 Single Linked List | sll.c, sll.h  | void * for generic type stored at each node.  
-Doubly Linked List | dll.c, dll.h  | void * for generic type stored at each node. 
+Doubly Linked List | dll.c, dll.h  | void * for generic type stored at each node.
--- include/dll.h
+++ include/dll.h
@@ -19,16 +19,24 @@ struct dll {
 };
 
 typedef struct dll dll_t;
+typedef struct Node node_t;
 
+// Constructors and destructors
 void init_dll(dll_t *);
 void dest_dll(dll_t *);
 
+// Accessors for first and last elements.
 void* first(dll_t *);
 void* last(dll_t *);
 
+// Insert functions at the head and tail.
 void ins_first(dll_t *);
 void ins_last(dll_t *);
 
+// Insert between utility function.
+void ins_btn(dll *, node_t *, node_t *);
+
+// Remove functions at the head and tail.
 void* rm_first(dll_t *);
 void* rm_last(dll_t *);
 

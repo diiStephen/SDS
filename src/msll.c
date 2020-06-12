bdd3d81054fa343b3aceac5633183b2b0d5c4bcf
dii <dstephenhono@gmail.com>
2020-06-12 18:11:03 -0400

    layout of msll.h.

--- src/msll.c
+++ src/msll.c
@@ -1,7 +1,7 @@
 #include "msll.h"
 
 
-int ins_first_(char **head, char **tail, int *size, int memsz,  char **sav) {
+int ins_first_(char **head, char **tail, int *size, int memsz, char **sav) {
   char *newest = calloc(1, memsz);
   if (newest == NULL) return -1;
   *sav = *head;

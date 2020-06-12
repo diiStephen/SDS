a5ec8ccaba14140017be59def829497ff2a39a41
dii <dstephenhono@gmail.com>
2020-06-12 18:26:33 -0400

    adding function prototypes to cll header.

--- include/cll.h
+++ include/cll.h
@@ -3,6 +3,9 @@
 
 /* Circularly Linked list implementation. */
 
+#define size(l)\
+  (l)->size
+
 struct node {
   void *data;
   struct node *next;
@@ -16,18 +19,21 @@ struct cll {
 typedef struct cll cll_t;
 typedef struct node node_t;
 
+//Constructor and destructor functions.
+node_t* create_node(node_t *, void *);
+node_t* dest_node(node_t *);
 void init_cll(cll_t *);
 void dest_cll(cll_t *);
 
+//Accessor functions.
 void* first(cll_t *);
 void* last(cll_t *);
 
+//Update functions.
 void ins_first(cll_t *, void *);
 void ins_last(cll_t *, void *);
 void rotate(cll_t *);
-
 void* rm_first(cll_t *);
-
 
 
 #endif

e86356bd3b79fc10920b21701427072264e4cb38
dii <dstephenhono@gmail.com>
2020-06-12 18:14:56 -0400

    Node struct and cll struct.

--- include/cll.h
+++ include/cll.h
@@ -1,5 +1,19 @@
 #ifndef CLL_H
 #define CLL_H
 
+/* Circularly Linked list implementation. */ 
 
-#endif 
+struct node {
+  void *data;
+  struct node* next;
+};
+
+struct cll {
+  int size;
+  node_t *tail;
+};
+
+typedef struct cll cll_t;
+typedef struct node node_t;
+
+#endif

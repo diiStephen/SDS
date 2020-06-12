88adf8f6c261d06b092a0131984b6e7062d2dfbc
dii <dstephenhono@gmail.com>
2020-06-12 18:30:36 -0400

    Implementing create_node function.

--- src/cll.c
+++ src/cll.c
@@ -1,1 +1,14 @@
 #include "cll.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+node_t* create_node(node_t *n, void *d) {
+  node_t *newest = malloc(sizeof(node_t));
+  if(newest == NULL) {
+    fprintf(stderr, "could not allocate memory for new node!\n");
+    exit(1);
+  }
+  newest->next = n;
+  newest->data = d;
+  return newest; 
+}

a530e8112de1e0ffc9b71b83f9bf22b1ae2610d1
dii <dstephenhono@gmail.com>
2020-06-13 17:08:16 -0400

    Adding stack implementations.

---
+++ include/lstk.h
@@ -0,0 +1,18 @@
+#ifndef L_STACK_H
+#define L_STACK_H
+/* Linked List Based Stack Interface. */
+
+/*
+* A stack interface based on adapting the singly linked list
+* to the stack.
+*
+*
+*/
+
+#include "sll.h"
+
+struct Stack {
+  int size;
+}
+
+#endif

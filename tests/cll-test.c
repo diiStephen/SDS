b2da7f006f8074f4b6b8c2b8e39f09a0e8b40f24
dii <dstephenhono@gmail.com>
2020-06-13 14:56:04 -0400

    updating cll-test.

--- tests/cll-test.c
+++ tests/cll-test.c
@@ -5,6 +5,7 @@
 #define header(h)\
   printf("Test: %s\n", h)
 
+
 int main(void) {
 
   {
@@ -16,9 +17,17 @@ int main(void) {
       *p = i;
       ins_first(&l, (void*)p);
     }
+    node_t *walk = l.tail->next;
+    for(int j = 0; j < size(&l); j++) {
+      printf("Data: %d\n", *(int*)walk->data);
+      walk = walk->next;
+    }
     printf("First element: %d\n", *((int*)first(&l)));
     printf("Last element: %d\n", *((int*)last(&l)));
     rm_first(&l);
+    printf("First element: %d\n", *((int*)first(&l)));
+    printf("Last element: %d\n", *((int*)last(&l)));
+    rotate(&l);
     printf("First element: %d\n", *((int*)first(&l)));
     printf("Last element: %d\n", *((int*)last(&l)));
     dest_cll(&l);

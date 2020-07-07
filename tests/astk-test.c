aa94c1512118edad7389947776c0deef7e9a109a
dii <dstephenhono@gmail.com>
2020-07-07 09:14:17 -0400

    Adding test cases to astk-tester.

--- tests/astk-test.c
+++ tests/astk-test.c
@@ -1,22 +1,28 @@
 #include "astk.h"
+#include "tester.h"
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 int main(int argc, char** argv) {
 
-  astack_t s;
-  init_astk(&s, 100);
+  {
+    header("Test 1");
+    astack_t s;
+    init_astk(&s, 100);
 
-  push(&s, "Hello");
-  push(&s, "World");
+    push(&s, "Hello");
+    push(&s, "World");
 
-  printf("Size of s: %d\n", size(&s));
-  printf("Top of stack: %s\n", (char*)top(&s));
-  printf("Popped: %s\n", (char*)pop(&s));
-  printf("Popped: %s\n", (char*)pop(&s));
-  printf("Size of s: %d\n", size(&s));
+    c_assert(size(&s) == 2);
+    c_assert(strcmp((char*)top(&s), "World") == 0);
 
-  dest_astk(&s);
+    printf("Size of s: %d\n", size(&s));
+    dest_astk(&s);
+  }
+
+
+  print_result;
   return 0;
 
 }

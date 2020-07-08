9fd969639b4c232107a551e273c38440a01c3daa
dii <dstephenhono@gmail.com>
2020-07-08 14:59:15 -0400

    Added memory allocation for sll field of lq

--- src/lq.c
+++ src/lq.c
@@ -1,9 +1,15 @@
 #include "lq.h"
+#include<stdio.h>
 #include<stdlib.h>
 
 void init_lq(lqueue_t *q) {
+  if ( (q->data = malloc(sizeof(sll_t))) == NULL) {
+    fprintf(stderr, "Could not allocate linked list memory!\n");
+    exit(0);
+  }
   init_sll(q->data);
 }
+
 
 void dest_lq(lqueue_t *q) {
   dest_sll(q->data);
@@ -11,12 +17,9 @@ void dest_lq(lqueue_t *q) {
 }
 
 /* Accessor operations. */
-int size(lqueue_t *q) {
-  return q->data->size;
-}
-
-//void* front(lqueue_t *);
+int size(lqueue_t *q) { return q->data->size; }
+void* front(lqueue_t *q) { return first(q->data); }
 
 /* Mutator operations. */
-//void enqueue(lqueue_t *, void *);
-//void* dequeue(lqueue_t *);
+void enqueue(lqueue_t *q, void *d) { ins_last(q->data, d); }
+void* dequeue(lqueue_t *q) { return rm_first(q->data); }
--- tests/lq-test.c
+++ tests/lq-test.c
@@ -14,6 +14,24 @@ int main(void) {
     footer("BLOCK 1");
   }
 
+  {
+    header("BLOCK 2");
+    lqueue_t s;
+    init_lq(&s);
+    int *p = NULL;
+    for(int i = 0; i < 641; i++) {
+      p = malloc(sizeof(int));
+      *p = i;
+      enqueue(&s, p);
+    }
+    c_assert(size(&s) == 641);
+    c_assert(*(int*)front(&s) == 0);
+    dest_lq(&s);
+    c_assert(s.data == NULL);
+    footer("BLOCK 2");
+  }
+
+
   print_result;
   return 0;
 }

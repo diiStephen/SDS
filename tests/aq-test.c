177b75fe5fb54040401ce3bccb862d581916d29d
dii <dstephenhono@gmail.com>
2020-07-08 14:01:40 -0400

    Added Test case 2 to aqtest.

--- tests/aq-test.c
+++ tests/aq-test.c
@@ -23,6 +23,25 @@ int main(void) {
     footer("Test 1");
   }
 
+  {
+    header("Test 2");
+    aqueue_t s;
+    init_aq(&s, 10);
+    int *p = NULL;
+    for(int i = 0; i < 5; i++) {
+      p = malloc(sizeof(int));
+      *p = i;
+      enqueue(&s, p);
+    }
+    c_assert(*(int*)dequeue(&s) == 0);
+    c_assert(*(int*)dequeue(&s) == 1);
+    c_assert(*(int*)dequeue(&s) == 2);
+    c_assert(size(&s) == 2);
+    dest_aq(&s);
+    footer("Test 2");
+
+  }
+
   print_result;
 
   return 0;

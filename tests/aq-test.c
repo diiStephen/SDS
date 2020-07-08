569aa899d98644abca2cfb24a3106011e405e777
dii <dstephenhono@gmail.com>
2020-07-08 14:17:28 -0400

    Added test case 3 to aqtest.

--- tests/aq-test.c
+++ tests/aq-test.c
@@ -7,7 +7,7 @@
 int main(void) {
 
   {
-    header("Test 1");
+    header("BLOCK 1");
     aqueue_t s;
     init_aq(&s, CAPACITY);
     int *p = NULL;
@@ -20,11 +20,11 @@ int main(void) {
     c_assert(*(int*)front(&s) == 0);
     dest_aq(&s);
     c_assert(s.data == NULL);
-    footer("Test 1");
+    footer("BLOCK 1");
   }
 
   {
-    header("Test 2");
+    header("BLOCK 2");
     aqueue_t s;
     init_aq(&s, 10);
     int *p = NULL;
@@ -38,7 +38,24 @@ int main(void) {
     c_assert(*(int*)dequeue(&s) == 2);
     c_assert(size(&s) == 2);
     dest_aq(&s);
-    footer("Test 2");
+    footer("BLOCK 2");
+  }
+
+  {
+    header("BLOCK 3");
+    aqueue_t s;
+    init_aq(&s, 25);
+    enqueue(&s, "A");
+    enqueue(&s, "B");
+    enqueue(&s, "C");
+    dequeue(&s);
+    dequeue(&s);
+    dequeue(&s);
+    enqueue(&s, "D");
+    c_assert(size(&s) == 1);
+    c_assert(strcmp(dequeue(&s), "D") == 0);
+    dest_aq(&s);
+    footer("BLOCK 3");
   }
 
   print_result;
